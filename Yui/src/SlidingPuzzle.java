82f109438d9afc8af7425456e652f7d85b744cad
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2016-12-09 13:11:45 -0500

    Added header. - Hillary

--- Yui/src/SlidingPuzzle.java
+++ Yui/src/SlidingPuzzle.java
@@ -1,4 +1,8 @@
-
+/* Hillary Li
+ * APCS BTHS
+ * Last modified: 12/9/16
+ * Percentage done: 0.01%
+ */
 public class SlidingPuzzle
 {
 	private static int[] puzzleSolution = {0, 1, 2, 3, 4, 5, 6, 7, 8};
