9a74b98db6f158f4f3170b5e26ed14d93b21b277
BT_1N3_02 <BT_1N3_02@BT_1N3_02.bktech.local>
2016-12-13 13:39:48 -0500

    Abstraction? Kind of? - Hillary

--- Yui/src/SlidingPuzzle.java
+++ Yui/src/SlidingPuzzle.java
@@ -1,39 +1,51 @@
 /* Hillary Li
  * APCS BTHS
  * Last modified: 12/9/16
- * Percentage done: 45%
+ * Percentage done: 51%
  */
+import java.util.*;
 public class SlidingPuzzle
 {
-	private static int[] puzzleSolution = {0, 1, 2, 3, 4, 5, 6, 7, 8};
-	public static int[] puzzleShuffled = puzzleSolution;
-	public static String difficulty = "easy";
-	private static int lengthOfPuzzle = 3 ;
-	public SlidingPuzzle(String input)
+	private int[] puzzleSolution = {0, 1, 2, 3, 4, 5, 6, 7, 8};
+	private  int[] puzzleShuffled = puzzleSolution;
+	private  String difficulty = "easy";
+	private int lengthOfPuzzle = 3 ;
+	SlidingPuzzle(String difficulty)
 	{
-		difficulty = input;
+		changeDifficulty(difficulty);
 		if (difficulty.equals("easy"))
 		{
-			lengthOfPuzzle = 3;
-			puzzleSolution = createSolution(lengthOfPuzzle);
-			puzzleShuffled = puzzleSolution;
+			initializePuzzle(3);
 		}
 		else if (difficulty.equals("medium"))
 		{
-			lengthOfPuzzle = 4;
-			puzzleSolution = createSolution(lengthOfPuzzle);
-			puzzleShuffled = puzzleSolution;
+			initializePuzzle(4);
 		}
 		else if(difficulty.equals("hard"))
 		{
-			lengthOfPuzzle = 5;
-			puzzleSolution = createSolution(lengthOfPuzzle);
-			puzzleShuffled = puzzleSolution;
+			initializePuzzle(5);
+		}
+		else
+		{
+			changeDifficulty("easy");
+			initializePuzzle(3);
+			System.out.print("Not a difficulty level, please type easy, medium, or hard: ");
 		}
 	}
-	private static String shufflePattern(int length)
+	private void initializePuzzle(int lengthOfPuzzle)
 	{
+		this.lengthOfPuzzle = lengthOfPuzzle;
+		puzzleSolution = createSolution(lengthOfPuzzle);
+		puzzleShuffled = puzzleSolution;
+		shufflePuzzle(shufflePattern());
 	}
+	public void changeDifficulty(String difficulty)
+	{
+		this.difficulty = difficulty;
+	}
+	//private String shufflePattern()
+	//{
+	//}
 	private int[] createSolution(int length)
 	{
 		int[] newSolution = new int[(length^2)-1];
@@ -52,10 +64,10 @@ public class SlidingPuzzle
 		else
 		{
 			puzzleMove(pattern.charAt(0));
-			return shufflePattern(pattern.substring(1));
+			return shufflePuzzle(pattern.substring(1));
 		}
 	}
-	public static void puzzleMove(char move)
+	public void puzzleMove(char move)
 	{
 	}
 	public Boolean done()
