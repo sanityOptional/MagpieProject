968950dfb02ee9016a0c6a70f98e6cb779ac99b5
SINY <SINY@DESKTOP-8JP42U4.home>
2016-12-23 03:43:03 -0500

    attemps -sinyee

--- Yui/src/TicTacToe.java
+++ Yui/src/TicTacToe.java
@@ -1,90 +1,114 @@
 
-/* Sin Yee Cheung
- * APCS BTHS
- * Last modified: 12/9
- * Percentage done: 
- */
 
+import java.util.Random;
 import java.util.Scanner;
-public class TicTacToe {
-		public static char turn = 'X';
-		public static int row, col; //field
-		public static Scanner scan = new Scanner(System.in);
-		public static char[][]board;
-		
-	
-		private int [][] board;
-		private string playerIcon;
-		private boolean gameStart;
-		private boolean playerWin;
-		private boolean gameDone; 
-		
-		5public static void main (String[] args)
-		{ 
-			board = new char [3][3];
-			for (int i = 0; i<3; i++)
-			{
-			for (int j=0; j<3; j++)
-			{
-				board[i][j]= '_';
-			}
-			Play();
-		}
-			
-			
-		}
-		private static void Play(){
-			boolean playing = true;
-			displayBoard();
-			while(playing){
-				row = scan.nextInt()-1; //scan #
-				col = scan.nextInt()-1;
-				if (board[row][col] != ' ')
-					 System.out.print("\n!! The space is taken.!! \n PLEASE TRY AGAIN: "); // next line
-			      row = scan.nextInt()-1;
-			      col = scan.nextInt()-1;
-			    if (board[row][col] == ' ')
-			    	board[row][col] = turn;
-				if (gameOver(row,col))
-				{
-					playing=false;
-					System.out.print("Game Over! Player " + turn + "wins!");
-				}
-			      }
-			}
-			
-		
-		private static void displayBoard()
-		{
-			for (int i = 0; i <3; i++){
-				System.out.println();
-				for (int j=0; j<3; j++){
-					System.out.print(board[i][j] + " ");
-				}
-			}
-		}
 
+
+public class TicTacToe{
+	
+	private int[] board;
+	private int playerIcon;
+	private int botIcon;
+	private boolean gameStart;
+	private boolean playerWin;
+	private int  move, spaceIndex;
+	boolean gameDone;
+	boolean playerTurn;
+	public static Scanner scan = new Scanner(System.in);
+	
+	public TicTacToe(){
+		board = new int[9];
+		playerIcon = 1;
+		botIcon = 2;
+		gameStart = true;
+		playerWin = false;
+		gameDone = false;
+	}
+	public TicTacToe(String icon)
+	{
+		if (icon.equals("X"))
+		{
+			playerIcon = 1;
+			botIcon = 2;
+		}
+		else if (icon.equals("O")){
+			playerIcon = 2;
+			botIcon = 1;
+		}
+		else {
+			System.out.print("Invalid character");
+			icon = "X";
+			playerIcon = 1 ;
+		}
+			
+	}
+	public void playerMove(int move){
+		System.out.print("Please enter from 0-8");
+		spaceIndex = scan.nextInt(); // scan # 	   
+		illegalMove(spaceIndex);
+	  }
+	
+
+	Boolean illegalMove(int move){
+		if (board[move] != ' '  )
+		{ 
+			return false;	
+		    System.out.print("\n!! The space is taken.!! \n PLEASE TRY AGAIN: "); // next line
+		    spaceIndex = scan.nextInt();
+		    spaceIndex = move; 
+		    }	
+			return true;				
+	}
+	
+	public void botMove(){
+		int random = (int)(Math.random() * ((8 - 0) + 1));
+		spaceIndex = random; 
+		illegalMove(spaceIndex);
+		}
+	
+	
+	public int blokorkWinMove(){
+		int a1[] = {0,1,2} ; // horizontal r1
+		int a2[] =  {3,4,5}; //h r2
+		int a3[] =  {6,7,8}; // h r3
+		int a4 [] = {0,3,6};  // vertical c 1
+		int a6 [] = {1,4,7}; //  v c2
+		int a7 [] = {2,5,8}; // v c3
+		int a8 [] = { 0,4,8}; //diagonal
+		int a9 [] = {2,4,6}; //d2
+		
+		if (int a1)
 		
 		
 		
-		
-		public static boolean gameOver(int rMove, int cMove){
-			//Check perpendicular victory
-			if(board[0][cMove] == board[1][cMove]
-				&& board[0][cMove] == board[2][cMove])
-				return true;
-			if(board[rMove][0] == board[rMove][1]
-				&& board[rMove][0] == board[rMove][2])
-				return true;
-			//Check diagonal victory
-			if (board[0][0] == board[1][1] && board[0][0] == board[2][2]
-				&& board[1][1] != '_')
-				return true;
-			if (board[0][2] == board[1][1] && board[0][2] == board[2][0]
-				&& board[1][1] != '_')
-				return true;
-			return false;
+		}
 		
 	}
-
-}	
+		
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
